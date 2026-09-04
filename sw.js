1e9d64b1562f2e7be696541b7515d02af16fd02a
contaempresa243-eng <contaempresa243@gmail.com>
2026-09-04 23:42:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const NOME_CACHE = 'leitor-musica-v3';
+const NOME_CACHE = 'leitor-musica-v4';
 const ARQUIVOS_CACHE = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
